8c41e3212cafde2a756b3dec77ec5417d2a8fb90
Matthew Batson <mbatson@protonmail.com>
2022-01-23 22:14:44 +1100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  // let indexOfItemToRemove = array.indexOf(itemToRemove);
+  // array.splice(indexOfItemToRemove, 1);
+  // return array;
+  for (let itemToRemove of itemsToRemove) {
+    array = array.filter(item => item !== itemToRemove);
+  }
 
+  return array;
 };
 
 // Do not edit below this line
