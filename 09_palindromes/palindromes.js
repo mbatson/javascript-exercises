2e2743974ad3d8f01d66e847471739a9dd111208
Matthew Batson <mbatson@protonmail.com>
2022-01-30 19:31:55 +1100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  let currentString = string.toLowerCase().split('');
+  let currentStringStripped = [];
 
+  for (let character of currentString) {
+    if (alpha.includes(character)) {
+      currentStringStripped.push(character);
+    }
+  }
+
+  let reversed = currentStringStripped.slice().reverse();
+
+  return currentStringStripped.join('') === reversed.join('');
 };
 
 // Do not edit below this line
