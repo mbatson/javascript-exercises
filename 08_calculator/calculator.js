56dc38fd6e169490fcf4dc6d8911fd4ef10924f6
Matthew Batson <mbatson@protonmail.com>
2022-01-30 17:45:12 +1100

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  // let sum = 0;
+
+  // for (let num of array) sum += num;
+
+  // return sum;
+  if (array.length > 0) {
+    return array.reduce((sum, num) => sum += num);
+  } else return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  if (nums.length > 0) {
+    return nums.reduce((sum, num) => sum *= num);
+  } else return 0;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+
+  let factorial = 1;
+  for (let factor = 1; factor <= num; factor++) {
+    factorial *= factor;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
