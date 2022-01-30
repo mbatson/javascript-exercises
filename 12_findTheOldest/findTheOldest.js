eb2275c297177bd6a96c03f10a98053a4132e264
Matthew Batson <mbatson@protonmail.com>
2022-01-30 23:03:53 +1100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  const oldestToYoungest = people.sort((previousPerson, currentPerson) => {
+    let previousYearsLived, currentYearsLived;
+    
+    if (!previousPerson.yearOfDeath) {
+      previousYearsLived = currentYear - previousPerson.yearOfBirth;
+    } else {
+      previousYearsLived = previousPerson.yearOfDeath - previousPerson.yearOfBirth;
+    }
+
+    if (!currentPerson.yearOfDeath) {
+      currentYearsLived = currentYear - currentPerson.yearOfBirth;
+    } else {
+      currentYearsLived = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    }
+    
+    return previousYearsLived > currentYearsLived ? -1 : 1;
+  });
+
+  return oldestToYoungest[0];
 };
 
 // Do not edit below this line
