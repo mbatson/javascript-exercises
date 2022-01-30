caa73a4ed1526d9539e2f192c1b0d3ef606c2034
Matthew Batson <mbatson@protonmail.com>
2022-01-30 20:17:55 +1100

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+  if (fib == 1 || fib == 2) return 1;
+  if (fib < 1) return "OOPS";
 
+  let num1 = 1, num2 = 1;
+  let currentNum;
+
+  for (let i = 3; i <= fib; i++) {
+    currentNum = num1 + num2;
+    num1 = num2;
+    num2 = currentNum;
+  }
+
+  return currentNum;
 };
 
 // Do not edit below this line
